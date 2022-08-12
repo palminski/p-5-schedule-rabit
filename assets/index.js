35d97785ad4d44f5b8fc4be9964946fbbf76614f
palminski <williambolls98@gmail.com>
2022-08-12 00:12:44 -0700

    Improved Appearence

--- assets/index.js
+++ assets/index.js
@@ -16,20 +16,23 @@ const saveTask = function() {
 
 let setUpTimecards = function() { //Sets up the timeblocks when the page loads
     let time = 9
+    let timeSuffix = "AM";
     for (i = 0; i<9 ;i++)
     {
         if (time > 12)
         {
             time = 1;
+            timeSuffix = "PM";
         }
         // Builds required elements for each timeblock
-        let $timeblock = $("<div>").attr("id","time-"+time).attr("data-array-slot",i).addClass("timeblock row bg-secondary my-2");
-        let $time = $("<h3>").text(time).addClass("block-time col-2 bg-white");
-        let $task = $("<p>").addClass("task-input  col-8");
+        let $timeblock = $("<div>").attr("id","time-"+time).attr("data-array-slot",i).addClass("timeblock row my-2");
+        let $time = $("<h3>").text(time+" "+timeSuffix).addClass("block-time col-2 bg-info text-center h-100 pt-3");
+        
+        let $task = $("<p>").addClass("task-input  col-8 pt-3");
         if (savedTasks[i]) {
             $task.text(savedTasks[i]);
         }
-        let $saveButton = $("<button>").html("<span class='oi oi-pin'></span>SAVE").addClass("col-2");
+        let $saveButton = $("<button>").html("<span class='oi oi-pin'></span>").addClass("col-2 btn btn-primary h-100");
 
         //places them on page
         $(".container").append($timeblock);
@@ -52,7 +55,7 @@ $(".timeblock").on("click","p",function(){ //When clicking on the text you can e
 
 $(".timeblock").on("blur","textarea",function(){ //When Clicking off text you will save it on page
     let text =$(this).val().trim();
-    let $task = $("<p>").text(text).addClass("task-input  col-8");
+    let $task = $("<p>").text(text).addClass("task-input  col-8 pt-3");
 
     $(this).replaceWith($task);
 });
